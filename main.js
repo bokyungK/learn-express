1349e9b7587c2300ad1001a3c08937c8a1fb6186
bokyungK <freebob2b@gmail.com>
2022-02-07 18:01:48 +0900

    update 기능을 express로 구현

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const sanitizeHtml = require('sanitize-html');
 const qs = require('querystring');
 
 app.get('/', (request, response) => {
-  fs.readdir('./data', function(error, filelist){
+  fs.readdir('./data', (error, filelist) => {
   const title = 'Welcome';
   const description = 'Hello, Node.js';
   const list = template.list(filelist);
@@ -21,7 +21,7 @@ app.get('/', (request, response) => {
 })
 
 app.get('/page/:pageId', (request, response) => {
-  fs.readdir('./data', function(error, filelist){
+  fs.readdir('./data', (error, filelist) => {
     const filteredId = path.parse(request.params.pageId).base;
     fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
       const title = request.params.pageId;
@@ -33,7 +33,7 @@ app.get('/page/:pageId', (request, response) => {
       const html = template.HTML(sanitizedTitle, list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
-          <a href="/update?id=${sanitizedTitle}">update</a>
+          <a href="/update/${sanitizedTitle}">update</a>
           <form action="delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
@@ -76,10 +76,54 @@ app.post('/create_process', (request, response) => {
       const description = post.description;
       fs.writeFile(`data/${title}`, description, 'utf8', err => {
         response.writeHead(302, {Location: `/page/${title}`});
-        response.end()
         // response.redirect(302, `/page/${title}`); express의 redirect 메소드로도 response 코드 구현 가능
       })
   })
+})
+
+app.get('/update/:pageId', (request, response) => {
+  fs.readdir('./data', function(error, filelist){
+    const filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`./data/${filteredId}`, 'utf8', (error2, description) => {
+      const title = request.params.pageId;
+      const list = template.list(filelist);
+      const html = template.HTML(title, list,
+        `
+        <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `,
+        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+      );
+      response.send(html);
+    });
+  });
+})
+
+app.post('/update_process', (request, response) => {
+  let body = '';
+  request.on('data', function(data){
+      body = body + data;
+  });
+  request.on('end', function(){
+      const post = qs.parse(body);
+      const id = post.id;
+      const title = post.title;
+      const description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function(error){
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {Location: `/update/${title}`});
+          response.end();
+        })
+      });
+  });
 })
 
 app.listen(port, () => {
@@ -99,47 +143,7 @@ app.listen(port, () => {
 //     } else if(pathname === '/create'){
 //     } else if(pathname === '/create_process'){
 //     } else if(pathname === '/update'){
-//       fs.readdir('./data', function(error, filelist){
-//         const filteredId = path.parse(queryData.id).base;
-//         fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-//           const title = queryData.id;
-//           const list = template.list(filelist);
-//           const html = template.HTML(title, list,
-//             `
-//             <form action="/update_process" method="post">
-//               <input type="hidden" name="id" value="${title}">
-//               <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-//               <p>
-//                 <textarea name="description" placeholder="description">${description}</textarea>
-//               </p>
-//               <p>
-//                 <input type="submit">
-//               </p>
-//             </form>
-//             `,
-//             `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-//           );
-//           response.writeHead(200);
-//           response.end(html);
-//         });
-//       });
 //     } else if(pathname === '/update_process'){
-//       let body = '';
-//       request.on('data', function(data){
-//           body = body + data;
-//       });
-//       request.on('end', function(){
-//           const post = qs.parse(body);
-//           const id = post.id;
-//           const title = post.title;
-//           const description = post.description;
-//           fs.rename(`data/${id}`, `data/${title}`, function(error){
-//             fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-//               response.writeHead(302, {Location: `/?id=${title}`});
-//               response.end();
-//             })
-//           });
-//       });
 //     } else if(pathname === '/delete_process'){
 //       let body = '';
 //       request.on('data', function(data){
