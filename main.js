47c8b479449a6669e2e6de6ba1610f79ee556882
bokyungK <freebob2b@gmail.com>
2022-02-08 19:41:41 +0900

    미들웨어 만들어보기: 중복된 코드 미들웨어로 정리

--- main.js
+++ main.js
@@ -11,63 +11,63 @@ const compression = require('compression');
 // 미들웨어
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
+app.get('*', (request, response, next) => {
+  fs.readdir('./data', (error, filelist) => {
+    request.list = filelist;
+    next();
+  })
+});
 
 app.get('/', (request, response) => {
-  fs.readdir('./data', (error, filelist) => {
   const title = 'Welcome';
   const description = 'Hello, Node.js';
-  const list = template.list(filelist);
+  const list = template.list(request.list);
   const html = template.HTML(title, list,
     `<h2>${title}</h2>${description}`,
     `<a href="/create">create</a>`
   )
   response.send(html);
-  })
 })
 
 app.get('/page/:pageId', (request, response) => {
-  fs.readdir('./data', (error, filelist) => {
-    const filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      const title = request.params.pageId;
-      const sanitizedTitle = sanitizeHtml(title);
-      const sanitizedDescription = sanitizeHtml(description, {
-        allowedTags:['h1']
-      })
-      const list = template.list(filelist);
-      const html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      )
-      response.send(html);
+  const filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    const title = request.params.pageId;
+    const sanitizedTitle = sanitizeHtml(title);
+    const sanitizedDescription = sanitizeHtml(description, {
+      allowedTags:['h1']
     })
+    const list = template.list(request.list);
+    const html = template.HTML(sanitizedTitle, list,
+      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+      ` <a href="/create">create</a>
+        <a href="/update/${sanitizedTitle}">update</a>
+        <form action="/delete_process" method="post">
+          <input type="hidden" name="id" value="${sanitizedTitle}">
+          <input type="submit" value="delete">
+        </form>`
+    )
+    response.send(html);
   })
 })
 
 app.get('/create', (request, response) => {
-  fs.readdir('./data', function(error, filelist){
-    const title = 'WEB - create';
-    const list = template.list(filelist);
-    const html = template.HTML(title, list,
-      `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-      `,
-      '')
-    response.send(html);
-  })
+  const title = 'WEB - create';
+  const list = template.list(request.list);
+  const html = template.HTML(title, list,
+    `
+    <form action="/create_process" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+    `,
+    '')
+  response.send(html);
 })
 
 app.post('/create_process', (request, response) => {
@@ -80,29 +80,27 @@ app.post('/create_process', (request, response) => {
 })
 
 app.get('/update/:pageId', (request, response) => {
-  fs.readdir('./data', function(error, filelist){
-    const filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`./data/${filteredId}`, 'utf8', (error2, description) => {
-      const title = request.params.pageId;
-      const list = template.list(filelist);
-      const html = template.HTML(title, list,
-        `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-      );
-      response.send(html);
-    });
-  });
+  const filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`./data/${filteredId}`, 'utf8', (error2, description) => {
+    const title = request.params.pageId;
+    const list = template.list(request.list);
+    const html = template.HTML(title, list,
+      `
+      <form action="/update_process" method="post">
+        <input type="hidden" name="id" value="${title}">
+        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+        <p>
+          <textarea name="description" placeholder="description">${description}</textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+      `,
+      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+    );
+    response.send(html);
+  })
 })
 
 app.post('/update_process', (request, response) => {
